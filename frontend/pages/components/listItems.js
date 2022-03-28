d03a3a0740bfb0af8113194eded6c5a197ba5a15
zzbgreatman <lzy968@gmail.com>
2022-03-27 21:10:30 -0700

    test listitem

--- frontend/pages/components/listItems.js
+++ frontend/pages/components/listItems.js
@@ -11,53 +11,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import SourceIcon from '@mui/icons-material/Source';
 
 export const mainListItems = (
-  <React.Fragment>
-    <ListItemButton>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <a href="/dashboard">
-        <ListItemText primary="Overview" />
-      </a>
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <SourceIcon />
-      </ListItemIcon>
-      <a href="/sourcespage">
-        <ListItemText primary="Sources" />
-      </a>
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <FileCopyIcon />
-      </ListItemIcon>
-      <a href="/snapshotspage">
-        <ListItemText primary="Snapshots" />
-      </a>    
-      </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <a href="/integrationspage">
-        <ListItemText primary="Integrations" />
-      </a> 
-    </ListItemButton>
-  </React.Fragment>
 );
 
 export const secondaryListItems = (
-  <React.Fragment>
-    <ListSubheader component="div" inset>
-    </ListSubheader>
-    <ListItemButton>
-      <ListItemIcon>
-        <SettingsIcon />
-      </ListItemIcon>
-      <a href="/">
-        <ListItemText primary="Setting" />
-      </a>
-    </ListItemButton>
-  </React.Fragment>
 );
