b835226e0e099a8336d91c8fd5f3629721b7dee1
zzbgreatman <lzy968@gmail.com>
2022-03-27 20:59:04 -0700

    111

--- frontend/pages/components/SideBar.js
+++ frontend/pages/components/SideBar.js
@@ -14,6 +14,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
+
 import { mainListItems, secondaryListItems } from './ListItems';
 import CustomSelects from './Simpleselect'
 
